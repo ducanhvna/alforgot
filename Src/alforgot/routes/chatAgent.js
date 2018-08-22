53abed7d790fe0c2570075c4436422207bfb545b
Nguyễn Đức Anh <ducanhvna@outlook.com>
2018-08-22 18:59:34 +0700

    update html

--- Src/alforgot/routes/chatAgent.js
+++ Src/alforgot/routes/chatAgent.js
@@ -6,11 +6,13 @@ function ChatAgent(name)
 
 ChatAgent.prototype.connection = function(socket){
     console.log('a user connected');
+    socket.broadcast.emit('hi');
     socket.on('disconnect',function(){
         console.log('User disconnect')
     });
     socket.on('chat message',function(msg){
         console.log('message: '+msg);
+        io.emit('chat message', msg);
     });
 }
 
